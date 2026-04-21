428e589a834735900a91fdd50f66c10ef8304c7c
MaksP740 <ponomarenkomaksim@ukr.net>
2026-04-21 22:36:20 +0300

    19

--- main.cpp
+++ main.cpp
@@ -62,15 +62,36 @@ int main() {
 
     // char C = '.';
     // string S = "hello world. hello step. hello it.";
-    // string S0 = "good";
+    // string S0 = " good";
     //
     // for (int i = 0; i < S.length(); i++) {
     //     if (S[i] == C) {
-    //        S = S.insert(i + 2, S0);
-    //         i += S.length();
+    //        S.insert(i, S0);
+    //         i += S0.length();
     //     }
     // }
     // cout << S << endl;
+
+    //=============== [ Task 5 ] ===================
+
+    // char C;
+    // string S, S0;
+    //
+    // cout << "Введіть рядок C: ";
+    // cin >> C;
+    // cin.ignore();
+    // cout << "Введіть рядок S: ";
+    // getline(cin, S);
+    // cout << "Введіть рядок S0: ";
+    // getline(cin, S0);
+    //
+    // for (int i = 0; i < S.length(); i++) {
+    //         if (S[i] == C) {
+    //            S.insert(i + 1, S0);
+    //             i += S0.length();
+    //         }
+    //     }
+    //     cout << S << endl;
 
     //=============== [ Task 6 ] ===================
 
@@ -90,9 +111,54 @@ int main() {
 
     //=============== [ Task 7 ] ===================
 
+    // string S;
+    // cout << "Введіть рядок S: ";
+    // getline(cin, S);
+    // string S0;
+    // cout << "Введіть рядок S0: ";
+    // getline(cin, S0);
+    //
+    // int res = 0;
+    //
+    //
+    // for (int i = 0; i <= S.length() - S0.length(); i++) {
+    //     if (S.substr(i, S0.length()) == S0) {
+    //         res++;
+    //     }
+    // }
+    // cout << "Кількість входжень: " << res << endl;
+
     //=============== [ Task 8 ] ===================
 
+    // string S;
+    // cout << "Введіть рядок S: ";
+    // getline(cin, S);
+    // string S0;
+    // cout << "Введіть рядок S0: ";
+    // getline(cin, S0);
+    //
+    // size_t res = S.find(S0);
+    //
+    // if (res != string::npos) {
+    //     S.erase(res, S0.length());
+    // }
+    // cout << "Результат: " << S << endl;
+
     //=============== [ Task 9 ] ===================
+
+    // string S;
+    // cout << "Введіть рядок S: ";
+    // getline(cin, S);
+    // string S0;
+    // cout << "Введіть рядок S0: ";
+    // getline(cin, S0);
+    //
+    // size_t res = 0;
+    //
+    // while ((res = S.find(S0, res)) != string::npos ) {
+    //     S.erase(res, S0.length());
+    // }
+    // cout << "Результат: " << S << endl;
 
     //=============== [ Task 10 ] ===================
 
@@ -116,9 +182,61 @@ int main() {
 
     //=============== [ Task 11 ] ===================
 
+    // string S = "hello world. hello step. hello it.";
+    // string S1 = "hello";
+    // string S2 = "hi" ;
+    //
+    // for (int i = 0; i <= S.length();) {
+    //
+    //     int pos = S.find(S1, i);
+    //     if (pos != -1) {
+    //         S.replace(pos, S1.length(), S2);
+    //         i = pos + S2.length();
+    //     }
+    //     else {
+    //         break;
+    //     }
+    // }
+    // cout <<"Результат: " << S << endl;
+
     //=============== [ Task 12 ] ===================
 
+    // string S = "Program";
+    //
+    // string res = "";
+    //
+    // for (int i = 0; i < S.length(); i += 2) {
+    //     res += S[i];
+    // }
+    //
+    // for (int i = S.length() - 1; i >= 0; i--) {
+    //     if (i % 2 != 0) {
+    //         res += S[i];
+    //     }
+    // }
+    //
+    // cout << "Результат: " << res << endl;
+
     //=============== [ Task 13 ] ===================
+
+    // string S = "Pormagr";
+    // int S1 = S.length();
+    //
+    // string N(S1, ' ');
+    //
+    // int k = 0;
+    //
+    // for (int i = 0; i < S1; i += 2) {
+    //     N[i] = S[k++];
+    // }
+    //
+    // for (int i = S1 - 1; i >= 0; i--) {
+    //     if (i % 2 == 1) {
+    //         N[i] = S[k++];
+    //     }
+    // }
+    //
+    // cout << N << endl;
 
     //=============== [ Task 14 ] ===================
 
@@ -156,46 +274,89 @@ int main() {
 
     //=============== [ Task 16 ] ===================
 
+    // string File1, File2, File3;
+    //
+    // ofstream in("SD.txt");
+    //
+    // if (in.is_open()) {
+    //
+    //     ifstream in1("SA.txt");
+    //     ifstream in2("SB.txt");
+    //     ifstream in3("SC.txt");
+    //
+    //     while (getline(in1, File1) && getline(in2, File2) && getline(in3, File3)) {
+    //
+    //         in << "-> " << File1 << " " << File2 << " " << File3 << endl;
+    //     }
+    //     in.close();
+    //     in1.close();
+    //     in2.close();
+    //     in3.close();
+    //     cout << "Файл успішно збережено!";
+    //
+    // }
+    // else {
+    //     cout << "Файл не відкривається!";
+    //     return 1;
+    // }
+
     //=============== [ Task 17 ] ===================
 
-    string line_text;
-
-    ifstream file("Task_17.txt");
-    if (file.is_open()) {
-
-        while (!file.eof()) {
-            // file >> line_text;
-            getline(file, line_text);
-            if (line_text.length() > 0) {
-                cout << line_text.substr(3, 2) << endl;
-                int m = atoi(line_text.substr(3, 2).c_str());
-            }
-        }
-        file.close();
-
-    }
+    // string line_text;
+    //
+    // ifstream file("Task_17.txt");
+    // if (file.is_open()) {
+    //
+    //     while (!file.eof()) {
+    //         // file >> line_text;
+    //         getline(file, line_text);
+    //         if (line_text.length() > 0) {
+    //             cout << line_text.substr(3, 2) << endl;
+    //             int m = atoi(line_text.substr(3, 2).c_str());
+    //         }
+    //     }
+    //     file.close();
+    //
+    // }
 
     //=============== [ Task 18 ] ===================
 
+    // ifstream fin("File17.txt");
+    // if (fin.is_open()) {
+    //
+    //
+    //     ofstream fin2("File_result.txt");
+    //     string date;
+    //
+    //     while (getline(fin, date)) {
+    //         if (date.length() >= 5) {
+    //             string month = date.substr(3,2);
+    //             if (month == "06" || month == "07" || month == "08") {
+    //                 fin2 << date << endl;
+    //             }
+    //         }
+    //     }
+    //     fin.close();
+    //     fin2.close();
+    //     cout << "Файл успішно збережено!";
+    // }
+    // else {
+    //     cout << "Файл не відкривається!";
+    //     return 1;
+    // }
+
+    //=============== [ Task 19 ] ===================
+
+    ifstream fin1("f1.txt");
+    ifstream fin2("f2.txt");
+
+    if (fin1.is_open() && fin2.is_open()) {
 
 
+    }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    //=============== [ Task 20 ] ===================
 
 
 
